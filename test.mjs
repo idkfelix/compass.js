e229a28e3b96d0256f8cad45e56419b4aa67a2b6
idkfelix <felix.coulthard@gmail.com>
2024-03-16 15:09:59 +1100

    .

--- test.mjs
+++
@@ -1,4 +0,0 @@
-import CompassClient from './src/index.js'
-
-CompassClient('mullauna-vic.compass.education','ASP.NET_SessionId=4f085678-db19-49ea-8a4e-39092336d83a')
-.then(client => console.log(client.userInfo))
